ff8d2311d1610c2c76bba0e791a9da7447bd5bf3
James Hales <james.devpipeline.com>
2023-02-16 14:33:22 -0700

    added challenge 3

---
+++ challenges/challenge3.py
@@ -0,0 +1,17 @@
+# Challenge: Write a program that sums all numbers in a list of numbers. You cannot use the built in "sum" function.
+# EX: list_ sum([1, 2, 3]) => 6
+
+
+
+def list_sum(working_list):
+  list_length = len(working_list)
+  working_num = 0
+
+  for i in range(0,list_length):
+    working_num =+ float(working_list[i])
+
+  return(working_num)
+
+working_list = [1,2,3]
+
+print(f'Sum of the working list is {list_sum(working_list)}')
