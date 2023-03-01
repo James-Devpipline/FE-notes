2037e0ef7ad053b00b7d6e8f51d84fa0947a31e2
James Hales <james.devpipeline.com>
2023-03-01 15:22:42 -0700

    added oop notes under js

---
+++ js/oop/script.js
@@ -0,0 +1,51 @@
+/* 
+OOP - Object Oriented Programming
+classes / objects via templates (blueprints)
+
+- declare
+- construct
+- instantiation
+- inherit
+- static vs instance methods
+- 
+
+JS Object factory pattern
+Syntactic sugar
+*/
+
+class User {
+  constructor(username) {
+    this.username = username
+  }
+}
+
+
+// function User(username) {
+//   this.username = username
+// }
+
+const user = {
+  username: "james"
+}
+
+const james = new User("james")
+
+console.log(user.username)
+console.log(james.username)
+
+
+
+class Admin extends User {
+  postGrades(student) {
+    console.log(`Student id ${student.id} is whack`)
+  }
+}
+
+const ryan = new Admin("RyGuy")
+const student = new User("SomeStudentFails@1")
+
+student.id = 5
+
+console.log(ryan.postGrades(student))
+
+console.log(typeof Admin)
