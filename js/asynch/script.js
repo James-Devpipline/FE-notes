f1aba9980e2d0f8fe7bccf447362ac08618eb3e6
james <james@devpipeline.com>
2023-03-06 16:19:18 -0700

    oh fetch!

--- js/asynch/script.js
+++ js/asynch/script.js
@@ -4,4 +4,17 @@ Asynchronous JavaScript
 - promises
 - Async functions
 - Await
+
+https://swapi.tech/documentation
 */
+
+let people;
+
+fetch("https://swapi.tech/api/people")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("From Within Fetch: ", data.results);
+    people = data.results;
+  }, 3000000);
+
+console.log("AFTER the fetch: ", people); // this happens before the fetch above
