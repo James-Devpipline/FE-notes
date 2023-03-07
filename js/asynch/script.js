6e86e62eef6da5531a90bd9ab25f836368cc3ccc
james <james@devpipeline.com>
2023-03-07 14:30:43 -0700

    more notes

--- js/asynch/script.js
+++ js/asynch/script.js
@@ -25,3 +25,31 @@ setTimeout(() => {
   people = ["john", "shawn", "denise"];
   console.log(peepole);
 }, 3000);
+
+// JS Event Loop
+
+// promises
+// promises states -> pending, fulfilled, rejected
+// promises chaining -> promise, .then(cb), .catch(error cb), .finally(cb)
+let shouldResolve = true;
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (shouldResolve) {
+      resolve("howdy");
+    } else {
+      reject("I'm not supposed to resolve, sorry");
+    }
+  }, 2000);
+});
+
+myPromise
+  .then((greeing) => {
+    return greeting[0].toUpperCase() + greeting.slice(1);
+  })
+  .then((formattedGreeting) => {
+    console.log(formattedGreeting);
+    return "Boy, that was crazy. Oh yeah: " + formattedGreeting;
+  })
+  .then(console.log)
+  .catch((err) => console.log(err));
