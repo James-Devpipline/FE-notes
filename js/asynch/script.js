6966e73a15503e3526f2778499524805559ad414
james <james@devpipeline.com>
2023-03-08 14:40:55 -0700

    api response stuff

--- js/asynch/script.js
+++ js/asynch/script.js
@@ -10,91 +10,91 @@ https://swapi.tech/documentation
 JS Event Loop (research a little bit of this later)
 */
 
-let people;
+// let people;
 
-fetch("https://swapi.tech/api/people")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log("From Within Fetch: ", data.results);
-    people = data.results;
-  });
+// fetch("https://swapi.tech/api/people")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log("From Within Fetch: ", data.results);
+//     people = data.results;
+//   });
 
-console.log("AFTER the fetch: ", people); // this happens before the fetch above
+// console.log("AFTER the fetch: ", people); // this happens before the fetch above
 
-let peepole;
+// let peepole;
 
-setTimeout(() => {
-  people = ["john", "shawn", "denise"];
-  console.log(peepole);
-});
+// setTimeout(() => {
+//   people = ["john", "shawn", "denise"];
+//   console.log(peepole);
+// });
 
-///////// Promises /////////
-console.log("///////// Promises /////////");
-// Promise states -> pending, fulfilled, rejected
-// Promise chaining -> Promise, .then(callback), .catch(error callback), .finally(callback)
+// ///////// Promises /////////
+// console.log("///////// Promises /////////");
+// // Promise states -> pending, fulfilled, rejected
+// // Promise chaining -> Promise, .then(callback), .catch(error callback), .finally(callback)
 
-// Documentation on Promise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+// // Documentation on Promise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
 
-function successfulPromise(data) {
-  console.log(data);
-}
+// function successfulPromise(data) {
+//   console.log(data);
+// }
 
-function erroredPromise(err) {
-  console.error(err.message);
-}
+// function erroredPromise(err) {
+//   console.error(err.message);
+// }
 
-// class Promise {
-//   constructor(executor) {
-//     this.executor = executor;
+// // class Promise {
+// //   constructor(executor) {
+// //     this.executor = executor;
+// //   }
+// // }
+
+// let shouldResolve = false;
+
+// const myPromise = new Promise((resolve, reject) => {
+//   console.log("You should see me first");
+//   setTimeout(() => {
+//     if (shouldResolve) {
+//       resolve("howdy");
+//     } else {
+//       reject(new Error ("I'm not supposed to resolve, sorry"));
+//     } //can also create a new error with new Error("error text here")
+//   }, 2000);
+// });
+// // new is used to instantiate a new object. Generally is used like this, doesn't matter what the obj is
+// // below is what the above promise is basically doing
+// function myPromiseFunc() {
+//   console.log(
+//     "Then me Third (this console log is stored in the example function, lines above the second one)"
+//   );
+//   if (shouldResolve) {
+//     return "howdy from the example function";
+//   } else {
+//     return "oof from the example function";
 //   }
 // }
 
-let shouldResolve = false;
+// const promiseCallback = greeting => ({formatted: greeting[0].toUpperCase() + greeting.slice(1), greeting})
 
-const myPromise = new Promise((resolve, reject) => {
-  console.log("You should see me first");
-  setTimeout(() => {
-    if (shouldResolve) {
-      resolve("howdy");
-    } else {
-      reject(new Error ("I'm not supposed to resolve, sorry"));
-    } //can also create a new error with new Error("error text here")
-  }, 2000);
-});
-// new is used to instantiate a new object. Generally is used like this, doesn't matter what the obj is
-// below is what the above promise is basically doing
-function myPromiseFunc() {
-  console.log(
-    "Then me Third (this console log is stored in the example function, lines above the second one)"
-  );
-  if (shouldResolve) {
-    return "howdy from the example function";
-  } else {
-    return "oof from the example function";
-  }
-}
+// myPromise
+//   .then(promiseCallback)
+//   .then((data) => {
+//     console.log(data) // this prints the previous return above from the prev promise
+//     return "Boy, that was crazy. Oh yeah: " + data.formatted
+//   })
+//   .then(finalData => {
+//     console.log(finalData)
+//     throw new Error("Eh, end of the line and you should've been off a long time ago")
+//   })
+//   .catch((err) => console.error("My custom Error Log: ", err))
+// .finally(() => console.log("oof, doesn't matter to me ooweee"))
 
-const promiseCallback = greeting => ({formatted: greeting[0].toUpperCase() + greeting.slice(1), greeting})
-
-myPromise
-  .then(promiseCallback)
-  .then((data) => {
-    console.log(data) // this prints the previous return above from the prev promise
-    return "Boy, that was crazy. Oh yeah: " + data.formatted
-  })
-  .then(finalData => {
-    console.log(finalData)
-    throw new Error("Eh, end of the line and you should've been off a long time ago")
-  })
-  .catch((err) => console.error("My customer Error Log: ", err))
-.finally(() => console.log("oof, doesn't matter to me ooweee"))
-
-console.log(">>>>>> This is after the promise in the natural flow of code")
+// console.log(">>>>>> This is after the promise in the natural flow of code")
 
 
-console.log("Then me Second");
-console.log(myPromiseFunc());
-console.log("Then me Fourh, below where the example function is called");
+// console.log("Then me Second");
+// console.log(myPromiseFunc());
+// console.log("Then me Fourh, below where the example function is called");
 
 // .then takes in the resolved value from the previous promise
 // .catch takes in the rejection
@@ -114,5 +114,35 @@ console.log("Then me Fourh, below where the example function is called");
 //
 //
 //
-// ****************MAKE A PIZZA!******************
 
+// Api Response stuff
+console.log("///////// Api response stuff /////////")
+
+const myPromise = () => new Promise((res, rej) => setTimeout(() => res("Some Data has been resolved, can continue logging"), 4000))
+
+async function main() {
+  console.log("starting req...")
+  const apiResponse = await fetch("https://swapi.tech/api/planets")
+  console.log("Stripping Json")
+  const data = await apiResponse.json()
+  console.log(apiResponse)
+  console.log("Finished!!!")
+  console.log(data)
+  console.log("Wait to resolve before logging this")
+
+  const promiseData = await myPromise()
+
+  console.log(promiseData)
+
+
+  // apiResponse
+  //   .then(res => res.json())
+  //   .then(sonsole.log)
+  // this throws a typeerror error
+}
+
+main()
+
+// fetch('https://httpbin.org/get')
+//   .then(res => res.json())
+//   .then(console.log)
