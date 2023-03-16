0c0d7ef92b5f472d766d98bd21f52eca847893a4
james <james@devpipeline.com>
2023-03-15 19:47:49 -0600

    added more notes for dom on event listeners

--- js/dom/script.js
+++ js/dom/script.js
@@ -137,3 +137,77 @@ setTimeout(() => {
   // properties from css can be used in java script however they just need to be camalCased, intsead of seperated by a -
   //  example: background-color in JS would be backgroundColor
 }, 4000);
+
+/* 
+Event Listeners
+
+Inline -> html elements. prefixed with "on". ie: "onclick"
+element.addEventListener -> event type, handler
+
+cleanup listeners
+element.removeEventListener -> event type, handler 
+*/
+
+function myAlert() {
+  alert("Warning, idk!!!");
+}
+
+// [a, b, c].forEach(alertWarning);
+
+// const customWindow = {
+//   alert: function () {},
+// };
+
+// customWindow.alert = function () {};
+////////////// don't do this, it overrides globals, refer to lecture on 03/15/23 at 3:55pm
+
+const btn = document.getElementById("toggleBtn");
+const clearBtn = document.getElementById("removeBtn");
+// const handleBtnClick = (event) => {
+//   if (event.target.innerText === "On") {
+//     event.target.innerText = "Off";
+//   } else {
+//     event.target.innerText = "On";
+//   }
+// };
+
+// console.log(btn);
+
+// btn.addEventListener("click", function (event) {
+//   console.log(this);
+// });
+// btn.addEventListener("click", function (event) {
+//   console.log(event);
+// });
+btn.addEventListener("click", function (event) {
+  console.log(this);
+  if (event.target.innerText === "On") {
+    event.target.innerText = "Off";
+  } else {
+    event.target.innerText = "On";
+  }
+});
+////////////// to fix these notes rewatch the lecture from 03/15/23 at 4:25 for the clear listener
+// btn.addEventListener("click", (event) => {
+//   console.log(this);
+//   if (event.target.innerText === "On") {
+//     event.target.innerText = "Off";
+//   } else {
+//     event.target.innerText = "On";
+//   }
+// });
+///////// changing it to a arrow function changes the scope
+
+// document.addEventListener("click", function (event) {
+//   if (event.target.innerText === "On") {
+//     event.target.innerText = "Off";
+//   } else {
+//     event.target.innerText = "On";
+//   }
+// });
+/////// this targets everything to change it to say either on or off
+btn.addEventListener("click");
+
+clearBtn.addEventListener("click", () => {
+  btn, removeEventListener("click", addEventListener);
+});
